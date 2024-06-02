0152bedc0ea9fb3aa38e57df1719c2dc796d1056
galihpermana29 <101629052+galihpermana292@users.noreply.github.com>
2024-06-02 15:17:54 +0700

    initial configurations

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-explicit-any": 1,
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "no-mixed-spaces-and-tabs": 1,
+    },
+  },
+];
