49293f9af9c89cf5571ca259b7b4321854373474
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2024-06-04 17:01:28 +0700

    feat: Create ProductGetDetailLoading Skeleton

--- src/app/product/[productId]/loading.tsx
+++ src/app/product/[productId]/loading.tsx
@@ -5,13 +5,25 @@ import React from 'react';
 
 const ProductDetailLoading = () => {
   return (
-    <div>
-      <div className="flex gap-[12px]">
-        <Skeleton.Node active />
-        <Skeleton.Node active />
-        <Skeleton.Node active />
-        <Skeleton.Node active />
+    <div className="w-full h-screen">
+      <div className="flex items-center gap-3 p-4">
+        <Skeleton.Avatar />
+        <Skeleton.Input />
       </div>
+      <div className="flex flex-col items-center justify-center ">
+        <Skeleton.Input size="large" />
+        <Skeleton.Input size="large" />
+        <Skeleton.Input size="large" />
+        <Skeleton.Input size="large" />
+      </div>
+      <Skeleton paragraph className="mt-5" />
+      <div className="flex items-center gap-2 mt-5">
+        <Skeleton.Node />
+        <Skeleton.Node />
+        <Skeleton.Node />
+        <Skeleton.Node />
+      </div>
+      <Skeleton paragraph={{ rows: 6 }} className="mt-5" />
     </div>
   );
 };
