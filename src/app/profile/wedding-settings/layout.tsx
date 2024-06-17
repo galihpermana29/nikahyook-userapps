0b9085d8dd0e2cd06a9ac025b625a612f091fc6b
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-06-17 09:23:11 +0800

    feat: `edit profile` and `wedding settings` loading state

--- src/app/profile/wedding-settings/layout.tsx
+++ src/app/profile/wedding-settings/layout.tsx
@@ -2,6 +2,7 @@ import DetailTitle from '@/shared/container/DetailHeader/DetailTitle';
 import CustomErrorBoundary from '@/shared/container/ErrorBoundary/ErrorBoundary';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import WeddingSettingsFormLoading from './container/WeddingSettingsFormLoading';
 
 export default function WeddingSettingsRootLayout({
   children,
@@ -10,7 +11,7 @@ export default function WeddingSettingsRootLayout({
 }>) {
   return (
     <ErrorBoundary FallbackComponent={CustomErrorBoundary}>
-      <Suspense fallback={null}>
+      <Suspense fallback={<WeddingSettingsFormLoading />}>
         <main className="flex flex-col size-full min-h-screen gap-5">
           <DetailTitle title="Wedding Settings" />
           <div className="pb-5 px-4 flex-grow flex">{children}</div>
