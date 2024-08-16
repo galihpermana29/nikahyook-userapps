1f1766ac0a987cea66c5afd592de7a4a71be061d
Afief Abdurrahman <mafiefa02@gmail.com>
2024-08-16 21:33:44 +0800

    fix: no background color (or transparent) on mark all as read button

--- src/app/notification/container/MarkAllAsRead.tsx
+++ src/app/notification/container/MarkAllAsRead.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Button } from 'antd';
 import React from 'react';
 import { useReadAllNotifications } from '../usecase/useReadAllNotifications';
 import { useGetNotificationsCount } from '@/shared/usecase/useGetNotificationsCount';
@@ -10,11 +9,10 @@ export const MarkAllAsRead = () => {
   const count = useGetNotificationsCount();
   if (count === 0) return;
   return (
-    <Button
+    <button
       onClick={() => readAll()}
-      type="link"
-      className="text-caption-2 w-full max-w-max text-right font-semibold text-ny-primary-500">
+      className="text-caption-2 px-4 w-full max-w-max text-right font-semibold size-full bg-white rounded-none text-ny-primary-500 hover:text-ny-primary-300 transition-all">
       MARK ALL AS READ
-    </Button>
+    </button>
   );
 };
