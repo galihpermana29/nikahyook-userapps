3431a3f50c5627d9cf40ea96242fd7fc80c39cb5
Afief Abdurrahman <mafiefa02@gmail.com>
2024-08-16 19:44:48 +0800

    fix: hydration error on client order date

--- src/app/order/(order-details)/details/[orderId]/container/OrderDate.tsx
+++ src/app/order/(order-details)/details/[orderId]/container/OrderDate.tsx
@@ -5,6 +5,8 @@ import customParseFormat from 'dayjs/plugin/customParseFormat';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import useMounted from '@/shared/usecase/useMounted';
+import { Skeleton } from 'antd';
 
 dayjs.extend(customParseFormat);
 dayjs.extend(utc);
@@ -12,6 +14,12 @@ dayjs.extend(timezone);
 dayjs.extend(relativeTime);
 
 export const OrderDate = ({ order_time }: { order_time: string }) => {
+  const mounted = useMounted();
+  if (!mounted)
+    return (
+      <Skeleton active className="h-[24px] w-36 bg-ny-primary-100 rounded-lg" />
+    );
+
   return (
     <span>
       {dayjs
